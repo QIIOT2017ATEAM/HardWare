10ab3cce3d9b7c2625d8738caaed482b7dbbf6c8
Unknown <shing31@naver.com>
2017-07-30 21:36:56 -0700

    test3

--- pa3-asynchronous-bluetooth-server-master/gossip_bt_server.py
+++ pa3-asynchronous-bluetooth-server-master/gossip_bt_server.py
@@ -55,6 +55,6 @@ if __name__ == '__main__':
             except Exception as e:
                 BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
                 client_handler.handle_close()
-
+    #testtestestestet
             # Sleep for 3 seconds
         sleep(3)
