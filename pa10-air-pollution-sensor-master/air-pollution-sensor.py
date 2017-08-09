4a80f1bbd4bd588411071b6d925d1f4caf70b586
Unknown <shing31@naver.com>
2017-08-09 13:57:23 -0700

    just test

--- pa10-air-pollution-sensor-master/air-pollution-sensor.py
+++ pa10-air-pollution-sensor-master/air-pollution-sensor.py
@@ -52,12 +52,20 @@ if __name__ == '__main__':
         msg = ""
         sensor_output = sensor_server.get_sensor_output()
         epoch_time = int(time())                    # epoch time
-        temp = sensor_output.get('Temp', -1)
-        SN1 = sensor_output.get('SN1', -1)
-        SN2 = sensor_output.get('SN2', -1)
-        SN3 = sensor_output.get('SN3', -1)
-        SN4 = sensor_output.get('SN4', -1)
-        PM25 = sensor_output.get('PM25', -1)
+#        temp = sensor_output.get('Temp', -1)
+#        SN1 = sensor_output.get('SN1', -1)
+#        SN2 = sensor_output.get('SN2', -1)
+#        SN3 = sensor_output.get('SN3', -1)
+#        SN4 = sensor_output.get('SN4', -1)
+#        PM25 = sensor_output.get('PM25', -1)
+        temp = uniform(20, 30)      # random temperature
+        SN1 = uniform(40, 50)       # random SN1 value
+        SN2 = uniform(60, 70)       # random SN2 value
+        SN3 = uniform(80, 90)       # random SN3 value
+        SN4 = uniform(100, 110)     # random SN4 value
+        PM25 = uniform(120, 130)    # random PM25 value
+
+
 
         r_msg = ""
         if args.output_format == "csv":
