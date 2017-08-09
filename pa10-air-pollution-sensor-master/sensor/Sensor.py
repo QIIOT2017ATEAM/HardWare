553f2de970473d84dadf55d2661d85bfd475c17d
Unknown <shing31@naver.com>
2017-08-08 23:57:11 -0700

    temp

--- pa10-air-pollution-sensor-master/sensor/Sensor.py
+++ pa10-air-pollution-sensor-master/sensor/Sensor.py
@@ -146,10 +146,10 @@ class SensorServer(Thread):
             #  n. set MUX to sensor n - 1, read sensor n - 1.
             logger.info("Reading {} sensor...".format(self.sensor_names[0]))
             # Temperature constant
-            t0 = 550
+            t0 = 510
             c0, c1 = self.read_sensor(0)
             # Channel 1 is not connected so we don't care about its output
-            temp = c0 - t0
+            temp = 1.22*c0 - t0
             logger.info("{} sensor outputs {} degree".format(self.sensor_names[0], temp))
             # Save output to the dict
             self.sensor_output[self.sensor_names[0]] = temp
