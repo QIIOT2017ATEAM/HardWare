ec866c61e840816d8c067bbdcfcbb42314a6d1f5
Unknown <shing31@naver.com>
2017-08-10 15:35:52 -0700

    1234

--- pa10-air-pollution-sensor-master/air-pollution-sensor.py
+++ pa10-air-pollution-sensor-master/air-pollution-sensor.py
@@ -53,19 +53,19 @@ if __name__ == '__main__':
         msg = ""
         sensor_output = sensor_server.get_sensor_output()
         epoch_time = int(time())                    # epoch time
-#        temp = sensor_output.get('Temp', -1)
-#        SN1 = sensor_output.get('SN1', -1)
-#        SN2 = sensor_output.get('SN2', -1)
-#        SN3 = sensor_output.get('SN3', -1)
-#        SN4 = sensor_output.get('SN4', -1)
-#        PM25 = sensor_output.get('PM25', -1)
-        temp = uniform(20, 30)      # random temperature
-        SN1 = uniform(0, 500)       # random SN1 value
-        SN2 = uniform(0, 500)       # random SN2 value
-        SN3 = uniform(0, 500)       # random SN3 value
-        SN4 = uniform(0, 500)     # random SN4 value
-        PM25 = uniform(0, 500)    # random PM25 value
- 
+        temp = sensor_output.get('Temp', -1)
+        SN1 = sensor_output.get('SN1', -1)
+        SN2 = sensor_output.get('SN2', -1)
+        SN3 = sensor_output.get('SN3', -1)
+        SN4 = sensor_output.get('SN4', -1)
+        PM25 = sensor_output.get('PM25', -1)
+#        temp = uniform(20, 30)      # random temperature
+#        SN1 = uniform(0, 50)       # random SN1 value
+#        SN2 = uniform(0, 500)       # random SN2 value
+#        SN3 = uniform(0, 500)       # random SN3 value
+#        SN4 = uniform(0, 500)     # random SN4 value
+#        PM25 = uniform(0, 500)    # random PM25 value
+
 
 
         r_msg = ""
@@ -129,7 +129,7 @@ if __name__ == '__main__':
                         # baud rate for HC-05 standard is 9600, so the time for the Bluetooth socket to process the
                         # string is (len(h_msg) + 2) * 8 / args.baud_rate; we add 10% margin to this time and wait for
                         # such a long time before we send the next row.
-                        sleep(((len(h_msg) + 2) * 8 * 1.1 / args.baud_rate))
+                        sleep(((len(h_msg) + 2) * 8 * 1.1 / int(args.baud_rate)))
 
                     # Send end-of-message indicator
                     print "\nINFO: Done"
